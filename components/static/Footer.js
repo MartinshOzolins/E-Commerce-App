f93628c3d16d4f62a85c893285449dd783e7601c
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-03-19 12:40:48 +0000

    add email address and phone links to the footer

--- components/static/Footer.js
+++ components/static/Footer.js
@@ -1,36 +1,40 @@
+import Link from "next/link";
+
 // # Server Component
 export default function Footer() {
   return (
     <footer className="pb-5 pt-16">
       <div className="bg-blue-100 h-[2px] " />
-      <div className="w-full pt-2 mx-auto flex flex-col sm:flex-row items-start justify-between px-2 sm:px-5">
-        {/* Customer Service */}
+      <div className="w-full pt-2 mx-auto flex flex-col sm:flex-row items-start justify-between px-2 sm:px-5 md:px-10">
         <div className="flex flex-col space-y-2 items-start text-sm sm:text-base">
           <h3 className="font-semibold text-blue-900 font-bold ">
             Customer Service
           </h3>
-          <p>📞 +1 234 567 890</p>
-          <p>📧 goodshub@example.com</p>
+
+          <p>
+            <a href="tel:+123456789">📞 +1 234 567 890</a>
+          </p>
+          <p>
+            <a href="mailto:someone@example.com">📧 goodshub@example.com</a>
+          </p>
           <p>📍 123 Fake Street, City, Country</p>
           <p>🕘 09:00 - 18:00</p>
         </div>
-
-        {/* Empty Middle Column */}
         <div></div>
-
-        {/* Links */}
-        <div className="space-y-2 text-right flex flex-col items-start sm:items-end pt-4 sm:pt-0">
-          <h3 className="font-bold text-blue-900">Quick Links</h3>
-          <a href="#" className="hover:text-blue-800">
-            Contacts
-          </a>
-          <a href="#" className="hover:text-blue-800">
-            Shipping
-          </a>
-          <a href="#" className="hover:text-blue-800">
-            Return Policy
-          </a>
-        </div>
+        <address className="not-italic">
+          <div className="space-y-2 text-right flex flex-col items-start sm:items-end pt-4 sm:pt-0">
+            <h3 className="font-bold text-blue-900">Quick Links</h3>
+            <Link href="#" className="hover:text-blue-800">
+              Contacts
+            </Link>
+            <Link href="#" className="hover:text-blue-800">
+              Shipping
+            </Link>
+            <Link href="#" className="hover:text-blue-800">
+              Return Policy
+            </Link>
+          </div>
+        </address>
       </div>
     </footer>
   );
