9d511ceee92d48152a66a58e9d73cbdbf77e3e35
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-05-11 21:13:07 +0100

    separate elements into reusable components

--- components/forms/AddressForm.js
+++ components/forms/AddressForm.js
@@ -1,50 +1,28 @@
 import AddressFormInput from "./AddressFormInput";
 
+const addressFields = [
+  { label: "House Info", name: "houseInfo" },
+  { label: "Street Name", name: "streetName" },
+  { label: "Town (optional)", name: "town" },
+  { label: "City", name: "city" },
+  { label: "County (optional)", name: "county" },
+  { label: "Postcode", name: "postcode" },
+  { label: "Country", name: "country" },
+];
+
 export default function AddressForm({ state, setState }) {
   return (
     <form className="flex flex-col space-y-2 pt-2 items-center w-full">
-      <AddressFormInput
-        label="House Info"
-        name="houseInfo"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="Street Name"
-        name="streetName"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="Town (optional)"
-        name="town"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="City"
-        name="city"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="County (optional)"
-        name="county"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="Postcode"
-        name="postcode"
-        state={state}
-        setState={setState}
-      />
-      <AddressFormInput
-        label="Country"
-        name="country"
-        state={state}
-        setState={setState}
-      />
+      {addressFields.map(({ label, name }) => (
+        <AddressFormInput
+          key={name}
+          label={label}
+          name={name}
+          state={state}
+          setState={setState}
+        />
+      ))}
     </form>
   );
 }
+s;
